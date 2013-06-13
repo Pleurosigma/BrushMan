59d9ec8300ca56e7af21f3753e0959fe9b552468
Logan Wilkerson <lcwilker@gmail.com>
2013-06-13 17:06:24 -0400

    Polygon docs
    Comments added to polygon class

--- src/collisions/Polygon.js
+++ src/collisions/Polygon.js
@@ -27,6 +27,13 @@ var BrushMan = BrushMan || {};
 	
 	p.vertices = null;
 	
+	/*
+	*	init
+	*	Initilizes the Polygon. Should only be called by constructor 
+	*	or children
+	*
+	*	@param {Array} Array of verticies
+	*/
 	p.init = function(vertices){
 		this.vertices = new Array();
 		for( var i in vertices){
@@ -34,9 +41,15 @@ var BrushMan = BrushMan || {};
 		}
 	}
 	
-	/**
-	*	addVertex(Array)
-	*	addVertex(x, y)
+	/*
+	*	addVertex
+	*	Adds a vertex to the polygon. This function accepts either
+	*	an array with [0] = x and [1] = y OR two parameters x and y
+	*
+	*	@param {Array} The vertex
+	*		OR
+	*	@param {Number} x component of the vertex
+	*	@param {Number} y component of the vertex
 	*/
 	p.addVertex = function(x, y){
 		if (x instanceof Array){
@@ -47,17 +60,37 @@ var BrushMan = BrushMan || {};
 		}
 	}
 	
+	/*
+	*	addVertices
+	*
+	*	Adds an array of verticies to the Polygon
+	*	@param {Array} Array of vertices
+	*/
 	p.addVertices = function(arguments){
 		for (var i in arguments){
 			this.vertices.push(arguments[i]);
 		}
 	}
 	
+	/*
+	*	removeVertex
+	*	
+	*	Removes a vertex based on the given index
+	*	@param {Number} The index of the vertex to remove
+	*/
 	p.removeVertex = function(index){
 		if (index in this.vertices)
-			this.vertices.splice(index, 1);
+			this.vertices.splice(index, 1);s
 	}
 	
+	/*
+	*	move
+	*	Moves the polygon by shifting the x and y value of each vertex
+	*	by dx and dy respectfully
+	*
+	*	@param {Number} dx
+	*	@param {Number} dy
+	*/
 	p.move = function(dx, dy){
 		for (var i in this.vertices){
 			this.vertices[i][0] += dx;
@@ -65,13 +98,26 @@ var BrushMan = BrushMan || {};
 		}
 	}
 	
+	/*
+	*	position
+	*	Positions the polygon so that the [0] and [1] value of 
+	*	this.vertices[0] is x and y respectfully
+	*
+	*	@param {Number} the x value
+	*	@param {Number} the y value
+	*/
 	p.position = function(x, y){
-		for (var i in this.vertices){
-			this.vertices[i][0] += x;
-			this.vertices[i][1] += y;
-		}
+		var dx = x - this.vertices[0][0];
+		var dy = y - this.vertices[0][1];
+		this.move(dx, dy);
 	}
 	
+	/*
+	*	clone
+	*	Returns a deep clone of the polygon
+	*
+	*	@return {BrushMan.Polygon} The clone.
+	*/
 	p.clone = function(){
 		var clone = new Polygon();
 		for (var i in this.vertices){
